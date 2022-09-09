c112f8d287775ea2392042185fbcff4bfd84a436
Declan Patullo <dpatullo@gmail.com>
2022-09-09 15:14:10 +0100

    add ID check to outage filter

--- routes/outages.js
+++ routes/outages.js
@@ -1,40 +1,39 @@
 const express = require('express');
 const axios = require('axios');
-const filterIds = require('../data_processing/filterIds')
+const filterIds = require('../data_processing/filterIds');
 
 const router = express.Router();
 
-const KRAKEN_FLEX_URL = "https://api.krakenflex.systems/interview-tests-mock-api/v1"
-const SITE_NAME = "norwich-pear-tree"
+const KRAKEN_FLEX_URL = 'https://api.krakenflex.systems/interview-tests-mock-api/v1';
+const SITE_NAME = 'norwich-pear-tree';
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-
   const outagesConfig = {
     method: 'get',
     url: `${KRAKEN_FLEX_URL}/outages`,
-    headers: { 'x-api-key' : process.env.KRAKEN_FLEX_API_KEY}
-  }
+    headers: { 'x-api-key': process.env.KRAKEN_FLEX_API_KEY },
+  };
 
   const siteInfoConfig = {
     method: 'get',
     url: `${KRAKEN_FLEX_URL}/site-info/${SITE_NAME}`,
-    headers: { 'x-api-key' : process.env.KRAKEN_FLEX_API_KEY}
-  }
+    headers: { 'x-api-key': process.env.KRAKEN_FLEX_API_KEY },
+  };
 
-  const getOutages = axios(outagesConfig)
+  const getOutages = axios(outagesConfig);
 
-  const getSiteInfo = axios(siteInfoConfig)
+  const getSiteInfo = axios(siteInfoConfig);
 
-  Promise.all([getOutages,getSiteInfo])
-  .then(
-    response => {
-      filterIds(response[0].data, response[1].data)
-    }
-  )
-  .catch()
+  Promise.all([getOutages, getSiteInfo])
+    .then(
+      (response) => {
+        filterIds(response[0].data, response[1].data);
+      },
+    )
+    .catch();
 
-  //res.send('respond with a resource');
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
