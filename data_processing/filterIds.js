dd31119f3a6045abc83bdcc635e3291ec2de0574
Declan Patullo <dpatullo@gmail.com>
2022-09-09 15:14:10 +0100

    add ID check to outage filter

--- data_processing/filterIds.js
+++ data_processing/filterIds.js
@@ -1,9 +1,16 @@
-const moment = require ('moment')
+const moment = require('moment');
 
 const filterIds = (outageData, siteInfo) => {
+  if (!siteInfo) {
+    return [];
+  }
 
-    const cutOffDate = moment('2022-01-01T00:00:00.000Z', moment.ISO_8601).format('X')
+  const cutOffDate = moment('2022-01-01T00:00:00.000Z', moment.ISO_8601).format('X');
 
-    return outageData.filter(outage => moment(outage.begin, moment.ISO_8601).format('X') >= cutOffDate)};
+  const validIds = siteInfo.devices.map((device) => device.id);
+
+  return outageData.filter((outage) => moment(outage.begin, moment.ISO_8601).format('X') >= cutOffDate
+                && validIds.includes(outage.id));
+};
 
 module.exports = filterIds;
