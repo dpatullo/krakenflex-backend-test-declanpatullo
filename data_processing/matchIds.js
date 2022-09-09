c42c503df53348f1807e17c5b3d9657ac7d3b0d6
Declan Patullo <dpatullo@gmail.com>
2022-09-09 14:43:49 +0100

    remove outages before cut off during matching

--- data_processing/matchIds.js
+++ data_processing/matchIds.js
@@ -1,7 +1,9 @@
-
+const moment = require ('moment')
 
 const matchIds = (outageData, siteInfo) => {
 
-};
+    const cutOffDate = moment('2022-01-01T00:00:00.000Z', moment.ISO_8601).format('X')
+
+    return outageData.filter(outage => moment(outage.begin, moment.ISO_8601).format('X') >= cutOffDate)};
 
 module.exports = matchIds;
